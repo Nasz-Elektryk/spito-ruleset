ba208f359d3e8d0493137f15d6507c7c6f090e61
Tomasz Kulig <pewienjaaa@gmail.com>
2024-01-19 10:08:19 +0100

    Create info.lua

---
+++ rules/info.lua
@@ -0,0 +1,8 @@
+function main()
+    api.info.Log("Example log info")
+    api.info.Debug("Example debug info")
+    api.info.Warn("Example warning info")
+    api.info.Important("Example important info")
+    api.info.Error("Example error info")
+    return false
+end
