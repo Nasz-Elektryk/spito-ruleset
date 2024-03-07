ab1fb747e84b6e892a4c8e64949ab20a5486fb4d
Maksymilian Gala <maksymilian.gala1@gmail.com>
2024-03-07 08:52:16 +0100

    fix: gnome_exists rule

--- rules/gnome_exists.lua
+++ rules/gnome_exists.lua
@@ -1,7 +1,7 @@
 function gnome_exists()
-  app_info, err = api.pkg.Get("gnome-shell")
+  app_info, err = api.pkg.get("gnome-shell")
   if err ~= nil then
-    api.info.Error("Error occured during obtaining package info!")
+    api.info.error("Error occured during obtaining package info!")
     return false
   end
   return true
